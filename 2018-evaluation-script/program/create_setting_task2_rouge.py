dfc710f20635e9269eb4e48227cd573e1c40f86e
Michihiro Yasunaga <gomagoma353@gmail.com>
2019-05-08 12:44:05 -0400

    updated eval scripts

--- 2018-evaluation-script/program/create_setting_task2_rouge.py
+++ 2018-evaluation-script/program/create_setting_task2_rouge.py
@@ -8,199 +8,237 @@ output_dir = sys.argv[2]
 model_type = sys.argv[3]
 print("creating setting: " + model_type)
 
-settings_string = """<ROUGE_EVAL version="1.5.5">
-    <EVAL ID="C98-1097">
+# paper_ids = ["A00-2018", "A00-2030", "A97-1014", "D09-1092", "D10-1044", "E03-1005", "J01-2004", "P04-1036", "P05-1013", "P08-1028", "P08-1043", "P08-1102","P11-1060", "P11-1061", "P87-1015", "W06-2932", "W06-3114", "W11-2123", "W99-0613", "W99-0623"]
+
+paper_ids = []
+for fname in os.listdir(input_dir +"/res/Task2"):
+    fname_paper = fname.split('.')[0]
+    paper_ids.append(fname_paper)
+
+settings_string = """<ROUGE_EVAL version="1.5.5">"""
+for paper_id in paper_ids:
+    one_setting = """<EVAL ID="%s">
         <PEER-ROOT>
             {input_dir}/res/Task2
         </PEER-ROOT>
-        
+
         <MODEL-ROOT>
             {input_dir}/ref/Task2
         </MODEL-ROOT>
-        
+
         <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
-        
+
         <PEERS>
-            <P ID="system">C98-1097.system.txt</P>
-        </PEERS>
-        
-        <MODELS>
-            <M ID="abstract">C98-1097.{model_type}.summary.txt</M>
-        </MODELS>
-    </EVAL>
-    <EVAL ID="D09-1023">
-        <PEER-ROOT>
-            {input_dir}/res/Task2
-        </PEER-ROOT>
-        
-        <MODEL-ROOT>
-            {input_dir}/ref/Task2
-        </MODEL-ROOT>
-        
-        <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
-        
-        <PEERS>
-            <P ID="system">D09-1023.system.txt</P>
-        </PEERS>
-        
-        <MODELS>
-            <M ID="abstract">D09-1023.{model_type}.summary.txt</M>
-        </MODELS>
-    </EVAL>
-    <EVAL ID="D10-1058">
-        <PEER-ROOT>
-            {input_dir}/res/Task2
-        </PEER-ROOT>
-        
-        <MODEL-ROOT>
-            {input_dir}/ref/Task2
-        </MODEL-ROOT>
-        
-        <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
-        
-        <PEERS>
-            <P ID="system">D10-1058.system.txt</P>
-        </PEERS>
-        
-        <MODELS>
-            <M ID="abstract">D10-1058.{model_type}.summary.txt</M>
-        </MODELS>
-    </EVAL>
-    <EVAL ID="N09-1001">
-        <PEER-ROOT>
-            {input_dir}/res/Task2
-        </PEER-ROOT>
-        
-        <MODEL-ROOT>
-            {input_dir}/ref/Task2
-        </MODEL-ROOT>
-        
-        <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
-        
-        <PEERS>
-            <P ID="system">N09-1001.system.txt</P>
-        </PEERS>
-        
-        <MODELS>
-            <M ID="abstract">N09-1001.{model_type}.summary.txt</M>
-        </MODELS>
-    </EVAL>
-    <EVAL ID="N09-1025">
-        <PEER-ROOT>
-            {input_dir}/res/Task2
-        </PEER-ROOT>
-        
-        <MODEL-ROOT>
-            {input_dir}/ref/Task2
-        </MODEL-ROOT>
-        
-        <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
-        
-        <PEERS>
-            <P ID="system">N09-1025.system.txt</P>
-        </PEERS>
-        
-        <MODELS>
-            <M ID="abstract">N09-1025.{model_type}.summary.txt</M>
-        </MODELS>
-    </EVAL>
-    <EVAL ID="P00-1025">
-        <PEER-ROOT>
-            {input_dir}/res/Task2
-        </PEER-ROOT>
-        
-        <MODEL-ROOT>
-            {input_dir}/ref/Task2
-        </MODEL-ROOT>
-        
-        <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
-        
-        <PEERS>
-            <P ID="system">P00-1025.system.txt</P>
-        </PEERS>
-        
-        <MODELS>
-            <M ID="abstract">P00-1025.{model_type}.summary.txt</M>
-        </MODELS>
-    </EVAL>
-    <EVAL ID="P07-1040">
-        <PEER-ROOT>
-            {input_dir}/res/Task2
-        </PEER-ROOT>
-        
-        <MODEL-ROOT>
-            {input_dir}/ref/Task2
-        </MODEL-ROOT>
-        
-        <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
-        
-        <PEERS>
-            <P ID="system">P07-1040.system.txt</P>
-        </PEERS>
-        
-        <MODELS>
-            <M ID="abstract">P07-1040.{model_type}.summary.txt</M>
-        </MODELS>
-    </EVAL>
-    <EVAL ID="W06-3909">
-        <PEER-ROOT>
-            {input_dir}/res/Task2
-        </PEER-ROOT>
-        
-        <MODEL-ROOT>
-            {input_dir}/ref/Task2
-        </MODEL-ROOT>
-        
-        <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
-        
-        <PEERS>
-            <P ID="system">W06-3909.system.txt</P>
-        </PEERS>
-        
-        <MODELS>
-            <M ID="abstract">W06-3909.{model_type}.summary.txt</M>
-        </MODELS>
-    </EVAL>
-    <EVAL ID="W09-0621">
-        <PEER-ROOT>
-            {input_dir}/res/Task2
-        </PEER-ROOT>
-        
-        <MODEL-ROOT>
-            {input_dir}/ref/Task2
-        </MODEL-ROOT>
-        
-        <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
-        
-        <PEERS>
-            <P ID="system">W09-0621.system.txt</P>
-        </PEERS>
-        
-        <MODELS>
-            <M ID="abstract">W09-0621.{model_type}.summary.txt</M>
-        </MODELS>
-    </EVAL>
-    <EVAL ID="W11-0815">
-        <PEER-ROOT>
-            {input_dir}/res/Task2
-        </PEER-ROOT>
-        
-        <MODEL-ROOT>
-            {input_dir}/ref/Task2
-        </MODEL-ROOT>
-        
-        <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
-        
-        <PEERS>
-            <P ID="system">W11-0815.system.txt</P>
-        </PEERS>
-        
-        <MODELS>
-            <M ID="abstract">W11-0815.{model_type}.summary.txt</M>
-        </MODELS>
-    </EVAL>
-</ROUGE_EVAL>
-"""
+            <P ID="system">%s.system.txt</P>
+        </PEERS>""" % (paper_id, paper_id)
+
+    model_setting = """<MODELS>"""
+    for ref_fname in os.listdir(input_dir +"/ref/Task2"):
+        ref_fname_paper = ref_fname.split('.')[0].split('_')[0]
+        ref_fname_summtype = ref_fname.split('.')[1]
+        if (ref_fname_paper == paper_id) and (ref_fname_summtype == model_type): # this is the right summary
+            model_setting += """<M ID="%s">%s</M>""" % (model_type, ref_fname)
+    model_setting += """</MODELS>"""
+
+    one_setting += model_setting
+    one_setting += """</EVAL>"""
+
+    settings_string += one_setting
+
+settings_string += """</ROUGE_EVAL>"""
+
+# settings_string = """<ROUGE_EVAL version="1.5.5">
+#     <EVAL ID="C98-1097">
+#         <PEER-ROOT>
+#             {input_dir}/res/Task2
+#         </PEER-ROOT>
+#
+#         <MODEL-ROOT>
+#             {input_dir}/ref/Task2
+#         </MODEL-ROOT>
+#
+#         <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
+#
+#         <PEERS>
+#             <P ID="system">C98-1097.system.txt</P>
+#         </PEERS>
+#
+#         <MODELS>
+#             <M ID="abstract">C98-1097.{model_type}.summary.txt</M>
+#         </MODELS>
+#     </EVAL>
+#     <EVAL ID="D09-1023">
+#         <PEER-ROOT>
+#             {input_dir}/res/Task2
+#         </PEER-ROOT>
+#
+#         <MODEL-ROOT>
+#             {input_dir}/ref/Task2
+#         </MODEL-ROOT>
+#
+#         <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
+#
+#         <PEERS>
+#             <P ID="system">D09-1023.system.txt</P>
+#         </PEERS>
+#
+#         <MODELS>
+#             <M ID="abstract">D09-1023.{model_type}.summary.txt</M>
+#         </MODELS>
+#     </EVAL>
+#     <EVAL ID="D10-1058">
+#         <PEER-ROOT>
+#             {input_dir}/res/Task2
+#         </PEER-ROOT>
+#
+#         <MODEL-ROOT>
+#             {input_dir}/ref/Task2
+#         </MODEL-ROOT>
+#
+#         <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
+#
+#         <PEERS>
+#             <P ID="system">D10-1058.system.txt</P>
+#         </PEERS>
+#
+#         <MODELS>
+#             <M ID="abstract">D10-1058.{model_type}.summary.txt</M>
+#         </MODELS>
+#     </EVAL>
+#     <EVAL ID="N09-1001">
+#         <PEER-ROOT>
+#             {input_dir}/res/Task2
+#         </PEER-ROOT>
+#
+#         <MODEL-ROOT>
+#             {input_dir}/ref/Task2
+#         </MODEL-ROOT>
+#
+#         <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
+#
+#         <PEERS>
+#             <P ID="system">N09-1001.system.txt</P>
+#         </PEERS>
+#
+#         <MODELS>
+#             <M ID="abstract">N09-1001.{model_type}.summary.txt</M>
+#         </MODELS>
+#     </EVAL>
+#     <EVAL ID="N09-1025">
+#         <PEER-ROOT>
+#             {input_dir}/res/Task2
+#         </PEER-ROOT>
+#
+#         <MODEL-ROOT>
+#             {input_dir}/ref/Task2
+#         </MODEL-ROOT>
+#
+#         <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
+#
+#         <PEERS>
+#             <P ID="system">N09-1025.system.txt</P>
+#         </PEERS>
+#
+#         <MODELS>
+#             <M ID="abstract">N09-1025.{model_type}.summary.txt</M>
+#         </MODELS>
+#     </EVAL>
+#     <EVAL ID="P00-1025">
+#         <PEER-ROOT>
+#             {input_dir}/res/Task2
+#         </PEER-ROOT>
+#
+#         <MODEL-ROOT>
+#             {input_dir}/ref/Task2
+#         </MODEL-ROOT>
+#
+#         <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
+#
+#         <PEERS>
+#             <P ID="system">P00-1025.system.txt</P>
+#         </PEERS>
+#
+#         <MODELS>
+#             <M ID="abstract">P00-1025.{model_type}.summary.txt</M>
+#         </MODELS>
+#     </EVAL>
+#     <EVAL ID="P07-1040">
+#         <PEER-ROOT>
+#             {input_dir}/res/Task2
+#         </PEER-ROOT>
+#
+#         <MODEL-ROOT>
+#             {input_dir}/ref/Task2
+#         </MODEL-ROOT>
+#
+#         <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
+#
+#         <PEERS>
+#             <P ID="system">P07-1040.system.txt</P>
+#         </PEERS>
+#
+#         <MODELS>
+#             <M ID="abstract">P07-1040.{model_type}.summary.txt</M>
+#         </MODELS>
+#     </EVAL>
+#     <EVAL ID="W06-3909">
+#         <PEER-ROOT>
+#             {input_dir}/res/Task2
+#         </PEER-ROOT>
+#
+#         <MODEL-ROOT>
+#             {input_dir}/ref/Task2
+#         </MODEL-ROOT>
+#
+#         <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
+#
+#         <PEERS>
+#             <P ID="system">W06-3909.system.txt</P>
+#         </PEERS>
+#
+#         <MODELS>
+#             <M ID="abstract">W06-3909.{model_type}.summary.txt</M>
+#         </MODELS>
+#     </EVAL>
+#     <EVAL ID="W09-0621">
+#         <PEER-ROOT>
+#             {input_dir}/res/Task2
+#         </PEER-ROOT>
+#
+#         <MODEL-ROOT>
+#             {input_dir}/ref/Task2
+#         </MODEL-ROOT>
+#
+#         <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
+#
+#         <PEERS>
+#             <P ID="system">W09-0621.system.txt</P>
+#         </PEERS>
+#
+#         <MODELS>
+#             <M ID="abstract">W09-0621.{model_type}.summary.txt</M>
+#         </MODELS>
+#     </EVAL>
+#     <EVAL ID="W11-0815">
+#         <PEER-ROOT>
+#             {input_dir}/res/Task2
+#         </PEER-ROOT>
+#
+#         <MODEL-ROOT>
+#             {input_dir}/ref/Task2
+#         </MODEL-ROOT>
+#
+#         <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
+#
+#         <PEERS>
+#             <P ID="system">W11-0815.system.txt</P>
+#         </PEERS>
+#
+#         <MODELS>
+#             <M ID="abstract">W11-0815.{model_type}.summary.txt</M>
+#         </MODELS>
+#     </EVAL>
+# </ROUGE_EVAL>"""
 
 result_settings = settings_string.format(**locals())
 
--- 2018-evaluation-script/program/task1_rouge_eval.py
+++ 2018-evaluation-script/program/task1_rouge_eval.py
@@ -1,11 +1,12 @@
 import sys
 import os
-import json
+import json, csv
 import subprocess
 
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 from copy import copy
+from task1_eval import parse, parse_csv
 
 def dictify(r,root=True):
     if root:
@@ -19,114 +20,6 @@ def dictify(r,root=True):
         d[x.tag].append(dictify(x,False))
     return d
 
-def parse(file):
-    print("parsing: " + str(file))
-    parse_data = {}
-    with open(file, "r") as f:
-        data = f.read().strip().split("\n")
-    for line in data:
-        line = line.strip()
-        if len(line) == 0:
-            continue
-        if line[-1] == "|":
-            line = line[0:-1]
-        # print("Old line: " + line)
-        line = line.replace("a | s, ", "a PIPE s, ")
-        # print("New line: " + line)
-        items = line.split(" | ")
-        line_data = {}
-        for kvpair in items:
-            if len(kvpair) == 0:
-                continue
-            # print kvpair
-            key = kvpair.strip().split(":", 1)[0].strip()
-            value = kvpair.strip().split(":", 1)[1].strip()
-            # print key + ":" + value
-            line_data[key] = value
-        if "Discourse Facet" not in line_data:
-            line_data["Discourse Facet"] = "None"
-
-        line_data["Reference Article"] = line_data["Reference Article"].replace(".xml", "")
-        line_data["Citing Article"] = line_data["Citing Article"].replace(".xml", "")
-        if line_data["Citation Marker Offset"].startswith("["):
-            line_data["Citation Marker Offset"] = line_data["Citation Marker Offset"][1:]
-        if line_data["Citation Marker Offset"].endswith("]"):
-            line_data["Citation Marker Offset"] = line_data["Citation Marker Offset"][:-1]
-        if line_data["Citation Marker Offset"].startswith("\'"):
-            line_data["Citation Marker Offset"] = line_data["Citation Marker Offset"][1:]
-        if line_data["Citation Marker Offset"].endswith("\'"):
-            line_data["Citation Marker Offset"] = line_data["Citation Marker Offset"][:-1]
-        if line_data["Citation Offset"].startswith("["):
-            line_data["Citation Offset"] = line_data["Citation Offset"][1:]
-        if line_data["Citation Offset"].endswith("]"):
-            line_data["Citation Offset"] = line_data["Citation Offset"][:-1]
-
-        if line_data["Reference Article"] not in parse_data:
-            parse_data[line_data["Reference Article"]] = {}
-        if line_data["Citing Article"] not in parse_data[line_data["Reference Article"]]:
-            parse_data[line_data["Reference Article"]][line_data["Citing Article"]] = {}
-        if line_data["Citation Marker Offset"] not in parse_data[line_data["Reference Article"]][line_data["Citing Article"]]:
-            parse_data[line_data["Reference Article"]][line_data["Citing Article"]][line_data["Citation Marker Offset"]] = {"original": line_data, "comparable": False}
-        ref_offset = line_data["Reference Offset"]
-        if ref_offset.startswith("["):
-            ref_offset = ref_offset[1:]
-        if ref_offset.endswith("]"):
-            ref_offset = ref_offset[:-1]
-        parsed_ref_offset_tmp = [x.strip() for x in ref_offset.split(",")]
-        print("\n\n")
-        print(parsed_ref_offset_tmp)
-        parsed_ref_offset = []
-        for ref in parsed_ref_offset_tmp:
-            print(ref)
-            if ref.startswith("\'") or ref.startswith("\""):
-                ref = ref[1:]
-            if ref.endswith("\'") or ref.endswith("\""):
-                ref = ref[:-1]
-            parsed_ref_offset.append(ref)
-        print(parsed_ref_offset)
-        # print("<root>" + line_data["Reference Text"] + "</root>")
-        line = "<root>" + line_data["Reference Text"] + "</root>"
-        # print("Line is:")
-        # print(line)
-        line = line.replace("&", "&amp;")
-        line = str(BeautifulSoup(line))
-        # line = line.replace("<\s>", "</s>")
-        # print("Line is:")
-        # print(line)
-        root = ET.fromstring(line)
-        ref_text_dict = dictify(root)
-        # print(ref_text_dict)
-        ref_text_dict_clean = {}
-        cnt = 0
-        for item in ref_text_dict["root"]["s"]:
-            cnt += 1
-            ref_text_dict_clean[item.get("sid", cnt)] = item["_text"]
-        parse_data[line_data["Reference Article"]][line_data["Citing Article"]][line_data["Citation Marker Offset"]]["Reference Text"] = ref_text_dict_clean
-        parse_data[line_data["Reference Article"]][line_data["Citing Article"]][line_data["Citation Marker Offset"]]["Reference Offset"] = parsed_ref_offset
-        ref_discourse_facet = line_data["Discourse Facet"]
-        parsed_discourse_facet = []
-        if len(ref_discourse_facet) > 0:
-            if ref_discourse_facet[0] == "[":
-                parsed_discourse_facet_tmp = [x.strip().lower().replace(" ", "_") for x in ref_discourse_facet[1:-1].split(",")]
-                parsed_discourse_facet = []
-                for ref in parsed_discourse_facet_tmp:
-                    if ref.startswith("\'") or ref.startswith("\""):
-                        ref = ref[1:]
-                    if ref.endswith("\'") or ref.endswith("\""):
-                        ref = ref[:-1]
-                    parsed_discourse_facet.append(ref)
-            else:
-                ref = ref_discourse_facet.lower().replace(" ", "_")
-                if ref.startswith("\'") or ref.startswith("\""):
-                    ref = ref[1:]
-                if ref.endswith("\'") or ref.endswith("\""):
-                    ref = ref[:-1]
-                parsed_discourse_facet.append(ref)
-        parse_data[line_data["Reference Article"]][line_data["Citing Article"]][line_data["Citation Marker Offset"]]["Discourse Facet"] = parsed_discourse_facet
-
-    # print(json.dumps(parse_data, sort_keys=True, indent=4))
-    # print("###################################################################################################################")
-    return parse_data
 
 def do_rouge(temp_dir, gold_file, submit_file):
     settings_string = """<ROUGE_EVAL version="1.5.5">
@@ -134,17 +27,17 @@ def do_rouge(temp_dir, gold_file, submit_file):
             <PEER-ROOT>
                 {temp_dir}
             </PEER-ROOT>
-            
+
             <MODEL-ROOT>
                 {temp_dir}
             </MODEL-ROOT>
-            
+
             <INPUT-FORMAT TYPE="SPL"></INPUT-FORMAT>
-            
+
             <PEERS>
                 <P ID="system">{gold_file}</P>
             </PEERS>
-            
+
             <MODELS>
                 <M ID="abstract">{submit_file}</M>
             </MODELS>
@@ -177,11 +70,12 @@ def do_rouge(temp_dir, gold_file, submit_file):
     return (p, r, f)
 
 def evaluate(gold_file, submit_file, temp_dir):
+    print("\n\n\n\n")
     print(gold_file)
     print(submit_file)
-    print("\n\n\n\n")
-    gold_data = parse(gold_file)
-    submit_data = parse(submit_file)
+
+    gold_data = parse_csv(gold_file)
+    submit_data = parse_csv(submit_file)
 
     precision_list = []
     recall_list = []
@@ -190,16 +84,24 @@ def evaluate(gold_file, submit_file, temp_dir):
     for ref_article in gold_data:
         for cit_article in gold_data[ref_article]:
             for cit_marker_offset in gold_data[ref_article][cit_article]:
-                gold_ref_text = gold_data[ref_article][cit_article][cit_marker_offset]["Reference Text"]
                 try:
+                    if "Reference Text" not in gold_data[ref_article][cit_article][cit_marker_offset]:
+                        print "no Reference Text in gold", ref_article
+                    gold_ref_text = gold_data[ref_article][cit_article][cit_marker_offset]["Reference Text"]
+                    if "Reference Text" not in submit_data[ref_article][cit_article][cit_marker_offset]:
+                        print "no Reference Text in submit", ref_article
+                    # try:
                     submit_ref_text = submit_data[ref_article][cit_article][cit_marker_offset]["Reference Text"]
-                except KeyError as e:
-                    # print("Data not found in submit file")
-                    # precision_list.append(0)
-                    # recall_list.append(0)
-                    # f1_list.append(0)
-                    # continue
-                    submit_ref_text = {}
+                    # except KeyError as e:
+                    #     print("Data not found in submit file")
+                    #     # precision_list.append(0)
+                    #     # recall_list.append(0)
+                    #     # f1_list.append(0)
+                    #     # continue
+                    #     submit_ref_text = {}
+                except:
+                    # print "No reference text for", ref_article
+                    continue
 
                 with open(os.path.join(temp_dir, "gold"), "w") as f:
                     print(gold_ref_text.values())
@@ -215,9 +117,11 @@ def evaluate(gold_file, submit_file, temp_dir):
                 recall_list.append(r)
                 f1_list.append(f)
 
-    avg_p = sum(precision_list) / float(len(precision_list))
-    avg_r = sum(recall_list) / float(len(recall_list))
-    avg_f = sum(f1_list) / float(len(f1_list))
+    if len(precision_list) == 0:
+        print "Length 0", gold_file
+    avg_p = sum(precision_list) / (float(len(precision_list)) + 10e-8)
+    avg_r = sum(recall_list) / (float(len(recall_list)) + 10e-8)
+    avg_f = sum(f1_list) / (float(len(f1_list)) + 10e-8)
     return (avg_p, avg_r, avg_f)
 
 def main(input_dir, temp_dir, output_dir):
@@ -225,12 +129,14 @@ def main(input_dir, temp_dir, output_dir):
     r_list = []
     f_list = []
     for gold_file in os.listdir(os.path.join(input_dir, "ref", "Task1")):
-        submit_file = os.path.join(input_dir, "res", "Task1", gold_file)
+        paper_id = gold_file.split('_')[0]
+        submit_file = os.path.join(input_dir, "res", "Task1", paper_id +".csv")
         if gold_file.startswith("."):
             continue
         if not os.path.exists(submit_file):
             continue
         (p, r, f1) = evaluate(os.path.join(input_dir, "ref", "Task1", gold_file), submit_file, temp_dir)
+        print p,r,f1
         # p = 0
         # r = 0
         # f1 = 0
