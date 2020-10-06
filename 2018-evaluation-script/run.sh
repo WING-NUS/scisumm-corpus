554a2dc0e27a614e02ede158617263a05e6905f8
cmuthuk <cmuthuk@amazon.com>
2020-10-06 03:27:55 -0700

    minor important fix to evaluation preproc and driver script

--- 2018-evaluation-script/run.sh
+++ 2018-evaluation-script/run.sh
@@ -3,6 +3,6 @@ for d in */ ; do
     cd "$d"
     echo "$pwd"
     rm *.log
-    bash program/run.sh input/ output/ program/ 1> out.log 2> err.log
+    bash program/run.sh input output/ program/ 1> out.log 2> err.log
     cd -
 done
