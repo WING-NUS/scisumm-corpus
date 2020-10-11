ee531b524f6016ad9bed818081807dfc2867a11c
MBcom <27956078+MBcom@users.noreply.github.com>
2020-10-11 15:52:17 +0200

    evaluation added

---
+++ CLSciSumm_2020_Evaluation/55- Martin-Luther-Universität Halle-Wittenberg/setup/ref/program/rouge/models/D31038.M.100.T.H
@@ -0,0 +1,6 @@
+Seven men have been arrested for the Aug. 7 bombings of the U.S. embassies in Kenya and Tanzania.
+Two, Rashid Saleh Hemed and Mustafa Mahmoud Said Ahmed, are in jail in Tanzania.
+Mohammed Saddiq Odeh and Mohamed Rashed Daoud al' Owhali are being held in isolation in a facility in New York.
+Three other suspects are being sought.
+Osama bin Laden was indicted on 238 counts in connection with the blasts that killed over 200 people and wounded over 5400.
+The U.S. announced a $5 million reward each for information leading to the capture or conviction of bin Laden, who is living in Afghanistan under Taliban protection, and Muhammed Atef, a chief lieutenant.
